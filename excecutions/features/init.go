812d40a872a9cc167b3d749f303a5278fc1cc070
Findryankp <findryankurnia@gmail.com>
2023-05-04 19:17:51 +0700

    features

--- excecutions/features/init.go
+++ excecutions/features/init.go
@@ -1,12 +1,19 @@
 package features
 
 import (
+	"github.com/Findryankp/cleanarchGo/excecutions/features/data"
+	"github.com/Findryankp/cleanarchGo/excecutions/features/delivery"
+	"github.com/Findryankp/cleanarchGo/excecutions/features/service"
 	"github.com/Findryankp/cleanarchGo/excecutions/generates"
 )
 
 func InitFeatures(featureName string) {
 	createFolderFeature(featureName)
 	entityCreate(featureName)
+
+	data.InitData(featureName)
+	delivery.InitDelivery(featureName)
+	service.InitService(featureName)
 }
 
 func createFolderFeature(featuresName string) {
