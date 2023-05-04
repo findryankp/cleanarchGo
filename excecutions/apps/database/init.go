e9e8535142e3dbf2d402b32a49b3d35288871ec3
Findryankp <findryankurnia@gmail.com>
2023-05-04 10:41:14 +0700

    database

--- excecutions/apps/database/init.go
+++ excecutions/apps/database/init.go
@@ -1,6 +1,16 @@
 package database
 
+import (
+	"github.com/Findryankp/cleanarchGo/excecutions/generates"
+)
+
 func InitDatabase() {
+	packagesInstall := []string{
+		"gorm.io/gorm",
+		"gorm.io/driver/mysql",
+	}
+	generates.PackageIntallList(packagesInstall)
+
 	mysqlConCreate()
 	migrationCreate()
 }
