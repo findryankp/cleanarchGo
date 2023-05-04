02196d33aa8e9b16afca51551103fed7bf1e1fad
Findryankp <findryankurnia@gmail.com>
2023-05-04 11:50:04 +0700

    init apps

--- excecutions/apps/initApps.go
+++ excecutions/apps/initApps.go
@@ -1,1 +1,15 @@
 package apps
+
+import (
+	"github.com/Findryankp/cleanarchGo/excecutions/apps/configs"
+	"github.com/Findryankp/cleanarchGo/excecutions/apps/database"
+	"github.com/Findryankp/cleanarchGo/excecutions/apps/middlewares"
+	"github.com/Findryankp/cleanarchGo/excecutions/apps/routes"
+)
+
+func InitApps() {
+	configs.InitConfig()
+	database.InitDatabase()
+	middlewares.InitMiddlewares()
+	routes.InitRoutes()
+}
