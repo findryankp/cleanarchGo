10d882aba9840d90f505d439c8bbb7036bb35c3c
Findryankp <findryankurnia@gmail.com>
2023-05-08 14:17:08 +0700

    fix handler

--- excecutions/features/delivery/handler.go
+++ excecutions/features/delivery/handler.go
@@ -48,10 +48,10 @@ func (h *Handler) GetAll(c echo.Context) error {
 	return c.JSON(http.StatusOK, helpers.ResponseSuccess("-", dataResponse))
 }
 
-func (t *Handler) GetById(c echo.Context) error {
+func (h *Handler) GetById(c echo.Context) error {
 	_id, _ := strconv.Atoi(c.Param("id"))
 	id := uint(_id)
-	dataCore, err := t.Service.GetById(id)
+	dataCore, err := h.Service.GetById(id)
 	if err != nil {
 		return c.JSON(http.StatusNotFound, helpers.ResponseFail("data not found"))
 	}
@@ -60,43 +60,43 @@ func (t *Handler) GetById(c echo.Context) error {
 	return c.JSON(http.StatusOK, helpers.ResponseSuccess("-", dataResponse))
 }
 
-func (t *Handler) Create(c echo.Context) error {
+func (h *Handler) Create(c echo.Context) error {
 	var request Request
 	if err := c.Bind(&request); err != nil {
 		return c.JSON(http.StatusBadRequest, helpers.ResponseFail("error bind data"))
 	}
 
-	dataResponse, err := t.Service.Create(RequestToCore(&request))
+	dataResponse, err := h.Service.Create(RequestToCore(&request))
 	if err != nil {
 		return c.JSON(http.StatusBadRequest, helpers.ResponseFail(err.Error()))
 	}
 
-	return c.JSON(http.StatusCreated, helpers.ResponseSuccess("Create Data Success", CoreToResponse(dataResponse)))
+	return c.JSON(http.StatusCreated, helpers.ResponseSuccess("create data success", CoreToResponse(dataResponse)))
 }
 
-func (t *Handler) Update(c echo.Context) error {
+func (h *Handler) Update(c echo.Context) error {
 	var request Request
 	if err := c.Bind(&request); err != nil {
 		return c.JSON(http.StatusBadRequest, helpers.ResponseFail("error bind data"))
 	}
 
 	id, _ := strconv.Atoi(c.Param("id"))
-	dataCore, err := t.Service.Update(RequestToCore(&request), uint(id))
+	dataCore, err := h.Service.Update(RequestToCore(&request), uint(id))
 	if err != nil {
 		return c.JSON(http.StatusNotFound, helpers.ResponseFail(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, helpers.ResponseSuccess("Update Data Success", CoreToResponse(dataCore)))
+	return c.JSON(http.StatusOK, helpers.ResponseSuccess("update data success", CoreToResponse(dataCore)))
 }
 
-func (t *Handler) Delete(c echo.Context) error {
+func (h *Handler) Delete(c echo.Context) error {
 	id, _ := strconv.Atoi(c.Param("id"))
 
-	if err := t.Service.Delete(uint(id)); err != nil {
+	if err := h.Service.Delete(uint(id)); err != nil {
 		return c.JSON(http.StatusNotFound, helpers.ResponseFail(err.Error()))
 	}
 
-	return c.JSON(http.StatusOK, helpers.ResponseSuccess("Delete Data Success", nil))
+	return c.JSON(http.StatusOK, helpers.ResponseSuccess("delete data success", nil))
 }
 	
 	`
